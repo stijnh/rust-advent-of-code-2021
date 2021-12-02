876ff8bd4221b6106e708301d9635e5049b20d6e
stijn <s.heldens@esciencecenter.nl>
2021-12-02 22:08:04 +0100

    Add solution for day 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod common;
 mod day01;
+mod day02;
 
 use common::*;
 use std::env;
@@ -7,7 +8,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() -> Result {
-    let funs = [day01::run];
+    let funs = [day01::run, day02::run];
 
     let mut args = env::args();
     let binary = args.next().unwrap_or_default();
@@ -23,7 +24,7 @@ fn main() -> Result {
         bail!("day must be a number between 1 and {}", funs.len());
     }
 
-    let input_file = format!("../inputs/day{:02}", day);
+    let input_file = format!("inputs/day{:02}", day);
     let f = File::open(&input_file).with_context(|| format!("failed to open: {}", input_file))?;
 
     let lines = BufReader::new(f)
